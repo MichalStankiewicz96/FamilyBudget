5dc92496c7d84bed6619f4bec46c59092da869e7
Michał Stankiewicz <michal.stankiewicz@herodot.com>
2023-09-21 16:19:36 +0200

    feat(fb-1000): configure program

--- src/FamilyBudget.Api/Program.cs
+++ src/FamilyBudget.Api/Program.cs
@@ -1,35 +1,57 @@
 
-namespace FamilyBudget.Api;
+using FamilyBudget.Application.Extensions;
+using Serilog;
 
-public class Program
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Information("Starting up");
+
+try
 {
-    public static void Main(string[] args)
+    RunApplication();
+}
+
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
+void RunApplication()
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.WebHost.CaptureStartupErrors(true);
+    // Add services to the container.
+    builder.Services.AddApplication(builder.Configuration);
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    ApplyMigrations(app);
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+
+void ApplyMigrations(WebApplication webApplication)
+{
+    //apply migrations
 }
